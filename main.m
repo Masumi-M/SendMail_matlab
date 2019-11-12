bca8c6a740e0108172678cded3732b0c7020dea9
Masumi.M <masumi.lacrosse@live.jp>
2019-11-12 12:29:48 +0900

    Add: sampleData and main

---
+++ main.m
@@ -0,0 +1,9 @@
+% ===== Info =====
+% Main Script for Send Mail
+% URL: https://www.umi-mori.jp/article/matlab/send_mail.php
+% Github: https://github.com/Masumi-M/SendMail_matlab
+
+% ===== Main =====
+attachmentFilePath = './sampleData.png';
+attachmentFileName = 'sampleData.png';
+sendMail(attachmentFilePath, attachmentFileName);
